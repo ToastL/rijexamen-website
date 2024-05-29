3a8d742c20aa2a712c4524c70ddf24fbebe6c3a4
Toaster5 <etiendenouden2006@gmail.com>
2024-05-29 11:45:40 +0200

    Interface updates & backend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import "./style.css";
+
+    import {Question, Questions} from "$lib/questions";
+
+    const questions = new Questions();
+    let currQuestion = questions.getQuestion();
+
+    const letters = "ABCD"
+</script>
+
+<section class="flex justify-center items-center h-screen bg-light-blue">
+    <main class="flex justify-evenly bg-white border-2 border-off-white rounded-xl">
+        <div class="block h-full w-full p-[20px]">
+            <div class="bg-white border-2 border-off-white rounded-xl overflow-hidden w-[400px] h-[300px]">
+                <img class="w-full h-full" src="../images/{currQuestion.getImage()}" alt="">
+            </div>
+        </div>
+        <div class="h-full w-full px-[60px] py-[30px]">
+            <h1>{currQuestion.getQuestion()}</h1>
+            <div class="flex flex-col justify-between h-full w-full">
+                {#each currQuestion.getOptions() as option, i}
+                    <button on:click={() => currQuestion = questions.getQuestion()} class="flex items-center bg-white border-2 border-off-white w-full h-[50px] p-[5px] rounded-xl">
+                        <span class="flex items-center justify-center bg-white border-2 border-off-white w-[36px] h-[36px] mr-[10px] rounded-md">
+                            <p>{letters[i]}</p>
+                        </span>
+                        <p>{option}</p>
+                    </button>
+                {/each}
+            </div>
+        </div>
+    </main>
+</section>
